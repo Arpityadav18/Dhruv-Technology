c05724935f53541fed1f0c9bc2ce02d42f33b964
Arpit Yadav <aarpityadav96@gmail.com>
2023-02-25 23:50:03 +0530

    first commit

---
+++ Dhruv Technology/Job Form/job.js
@@ -0,0 +1,296 @@
+// Mouse Circle
+const mouseCircle = document.querySelector(".mouse-circle");
+const mouseDot = document.querySelector(".mouse-dot");
+
+let mouseCircleBool = true;
+
+const mouseCircleFn = (x, y) => {
+  mouseCircleBool &&
+    (mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`);
+
+  mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
+};
+// End of Mouse Circle
+
+// Animated Circles
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
+
+let mX = 0;
+let mY = 0;
+const z = 100;
+
+const animateCircles = (e, x, y) => {
+  if (x < mX) {
+    circles.forEach((circle) => {
+      circle.style.left = `${z}px`;
+    });
+    mainImg.style.left = `${z}px`;
+  } else if (x > mX) {
+    circles.forEach((circle) => {
+      circle.style.left = `-${z}px`;
+    });
+    mainImg.style.left = `-${z}px`;
+  }
+
+  if (y < mY) {
+    circles.forEach((circle) => {
+      circle.style.top = `${z}px`;
+    });
+    mainImg.style.top = `${z}px`;
+  } else if (y > mY) {
+    circles.forEach((circle) => {
+      circle.style.top = `-${z}px`;
+    });
+    mainImg.style.top = `-${z}px`;
+  }
+
+  mX = e.clientX;
+  mY = e.clientY;
+};
+// End of Animated Circles
+
+
+let hoveredElPosition = [];
+
+const stickyElement = (x, y, hoveredEl) => {
+  // Sticky Element
+  if (hoveredEl.classList.contains("sticky")) {
+    hoveredElPosition.length < 1 &&
+      (hoveredElPosition = [hoveredEl.offsetTop, hoveredEl.offsetLeft]);
+
+    hoveredEl.style.cssText = `top: ${y}px; left: ${x}px`;
+
+    if (
+      hoveredEl.offsetTop <= hoveredElPosition[0] - 100 ||
+      hoveredEl.offsetTop >= hoveredElPosition[0] + 100 ||
+      hoveredEl.offsetLeft <= hoveredElPosition[1] - 100 ||
+      hoveredEl.offsetLeft >= hoveredElPosition[1] + 100 
+    ) {
+      hoveredEl.style.cssText = "";
+      hoveredElPosition = [];
+    }
+
+    hoveredEl.onmouseleave = () => {
+      hoveredEl.style.cssText = "";
+      hoveredElPosition = [];
+    };
+  }
+  // End of Sticky Element
+};
+
+
+// Progress Bar
+const sections = document.querySelectorAll("section");
+const progressBar = document.querySelector(".progress-bar");
+const halfCircles = document.querySelectorAll(".half-circle");
+const halfCircleTop = document.querySelector(".half-circle-top");
+const progressBarCircle = document.querySelector(".progress-bar-circle");
+
+let scrolledPortion = 0;
+let scrollBool = false;
+let imageWrapper = false;
+
+const progressBarFn = (bigImgWrapper) => {
+  imageWrapper = bigImgWrapper;
+  let pageHeight = 0;
+  const pageViewportHeight = window.innerHeight;
+
+  if (!imageWrapper) {
+    pageHeight = document.documentElement.scrollHeight;
+    scrolledPortion = window.pageYOffset;
+  } else {
+    pageHeight = imageWrapper.firstElementChild.scrollHeight;
+    scrolledPortion = imageWrapper.scrollTop;
+  }
+
+  const scrolledPortionDegree =
+    (scrolledPortion / (pageHeight - pageViewportHeight)) * 360;
+
+  halfCircles.forEach((el) => {
+    el.style.transform = `rotate(${scrolledPortionDegree}deg)
+`;
+
+    if (scrolledPortionDegree >= 180) {
+      halfCircles[0].style.transform = "rotate(180deg)";
+      halfCircleTop.style.opacity = "0";
+    } else {
+      halfCircleTop.style.opacity = "1";
+    }
+  });
+
+  scrollBool = scrolledPortion + pageViewportHeight === pageHeight;
+
+  // Arrow Rotation
+  if (scrollBool) {
+    progressBarCircle.style.transform = "rotate(180deg)";
+  } else {
+    progressBarCircle.style.transform = "rotate(0)";
+  }
+  // End of Arrow Rotation
+};
+
+// Progress Bar Click
+progressBar.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (!imageWrapper) {
+    const sectionPositions = Array.from(sections).map(
+      (section) => scrolledPortion + section.getBoundingClientRect().top
+    );
+
+    const position = sectionPositions.find((sectionPosition) => {
+      return sectionPosition > scrolledPortion;
+    });
+
+    scrollBool ? window.scrollTo(0, 0) : window.scrollTo(0, position);
+  } else {
+    scrollBool
+      ? imageWrapper.scrollTo(0, 0)
+      : imageWrapper.scrollTo(0, imageWrapper.scrollHeight);
+  }
+});
+// End of Progress Bar Click
+
+progressBarFn();
+
+// End of Progress Bar
+
+// Navigation
+const menuIcon = document.querySelector(".menu-icon");
+const navbar = document.querySelector(".navbar");
+
+const scrollFn = () => {
+  menuIcon.classList.add("show-menu-icon");
+  navbar.classList.add("hide-navbar");
+
+  if (window.scrollY === 0) {
+    menuIcon.classList.remove("show-menu-icon");
+    navbar.classList.remove("hide-navbar");
+  }
+
+  progressBarFn();
+};
+
+document.addEventListener("scroll", scrollFn);
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.remove("show-menu-icon");
+  navbar.classList.remove("hide-navbar");
+});
+// End of Navigation
+
+
+// Section 5
+// Form
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
+
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = `Your ${input.placeholder}`;
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+
+  input.addEventListener("blur", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = "Apply Now";
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+});
+// End of Form
+
+// Slideshow
+const slideshow = document.querySelector(".slideshow");
+
+setInterval(() => {
+  const firstIcon = slideshow.firstElementChild;
+
+  firstIcon.classList.add("faded-out");
+
+  const thirdIcon = slideshow.children[3];
+
+  thirdIcon.classList.add("light");
+
+  thirdIcon.previousElementSibling.classList.remove("light");
+
+  setTimeout(() => {
+    slideshow.removeChild(firstIcon);
+
+    slideshow.appendChild(firstIcon);
+
+    setTimeout(() => {
+      firstIcon.classList.remove("faded-out");
+    }, 500);
+  }, 500);
+}, 3000);
+// End of Slideshow
+
+// Form Validation
+const form = document.querySelector(".contact-form");
+const username = document.getElementById("name");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const resume = document.getElementById("resume");
+const message = document.getElementById("message");
+const messages = document.querySelectorAll(".message");
+
+const error = (input, message) => {
+  input.nextElementSibling.classList.add("error");
+  input.nextElementSibling.textContent = message;
+};
+
+const success = (input) => {
+  input.nextElementSibling.classList.remove("error");
+};
+
+const checkRequiredFields = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      error(input, `${input.id} is required`);
+    }
+  });
+};
+
+const checkLength = (input, min) => {
+  if (input.value.trim().length < min) {
+    error(input, `${input.id} must be at least ${min} characters`);
+  } else {
+    success(input);
+  }
+};
+
+const checkEmail = (input) => {
+  const regEx =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (regEx.test(input.value.trim())) {
+    success(input);
+  } else {
+    error(input, "Email is not valid");
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  checkLength(username, 2);
+  checkLength(phone, 2);
+  checkLength(subject, 2);
+  checkLength(resume, 2);
+  checkLength(message, 10);
+  checkEmail(email);
+  checkRequiredFields([username, phone, email, subject, resume, message]);
+
+  const notValid = Array.from(messages).find((message) => {
+    return message.classList.contains("error");
+  });
+
+  notValid && e.preventDefault();
+});
+// End of Form Validation
+// End of Section 5
